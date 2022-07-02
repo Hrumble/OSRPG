4e79e9014c9292bd66639e4d3798363f3503c1f0
Hrumble <nolan.gero@gmail.com>
2022-07-03 05:48:01 +0700

    Better Interpreter

--- Interpreter.py
+++ Interpreter.py
@@ -1,8 +1,21 @@
 
 def StartGame():
     while True:
-        playerInput = input("> ")
+        playerInput = input("> ").split()
+        func = playerInput[0]
+        command = func + "("
+        if not len(playerInput) < 1:
+            args = playerInput[1:]
+            for i in range(len(args)):
+                if i == 0:
+                    command += args[i]
+                else:
+                    command += ", " + args[i]
+        command += ")"
+        print(command)
+
         try:
-            eval(playerInput)
+            eval(command)
         except:
             print("[SYSTEM] Your Command Was Not Recognized")
+
